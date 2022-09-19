461b28fe4f40e467779302ab3a10b99685680fda
piter26133 <65885814+piter26133@users.noreply.github.com>
2022-09-19 12:26:11 +0200

    static website test3

--- script.sh
+++ script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "Static websites ON"
 az storage blob service-properties update \
-   --account-name "ppaszek123" \
-   --static-website \
-   --404-document "error.html" \
-   --index-document "index.html" \
-   --verbose \
-   -o table
+    --auth-mode "login" \
+    --account-name "ppaszek123" \
+    --static-website \
+    --404-document "error.html" \
+    --index-document "index.html" \
+    --verbose \
+    -o table
