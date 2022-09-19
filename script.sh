a93092acd160ec2113097fff7f5cf8e5738d49b4
piter26133 <65885814+piter26133@users.noreply.github.com>
2022-09-19 11:50:15 +0200

    static website bicep test2

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Static websites ON"
+az storage blob service-properties update \
+   --account-name "ppaszek123" \
+   --static-website \
+   --404-document "error.html" \
+   --index-document "index.html" \
+   --verbose \
+   -o table
